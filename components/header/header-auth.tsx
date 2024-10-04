4e62b10eb233900141f384bc93661e6e66895cd1
Sam Mead <samjamead@gmail.com>
2024-10-04 21:46:23 +0100

    Dropdown signout ting

--- components/header/header-auth.tsx
+++ components/header/header-auth.tsx
@@ -1,49 +1,40 @@
 import { signOutAction } from '@/app/actions';
-import { hasEnvVars } from '@/utils/supabase/check-env-vars';
 import Link from 'next/link';
-import { Badge } from '../ui/badge';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
 import { Button } from '../ui/button';
 import { User } from '@supabase/supabase-js';
 
 export default async function AuthButton({ user }: { user: User }) {
-  if (!hasEnvVars) {
-    return (
-      <>
-        <div className='flex items-center gap-4'>
-          <div></div>
-          <div className='flex gap-2'>
-            <Button
-              asChild
-              size='sm'
-              variant={'outline'}
-              disabled
-              className='pointer-events-none cursor-none opacity-75'
-            >
-              <Link href='/sign-in'>Sign in</Link>
-            </Button>
-            <Button
-              asChild
-              size='sm'
-              variant={'default'}
-              disabled
-              className='pointer-events-none cursor-none opacity-75'
-            >
-              <Link href='/sign-up'>Sign up</Link>
-            </Button>
-          </div>
-        </div>
-      </>
-    );
-  }
   return user ? (
-    <div className='flex items-center gap-4 text-sm'>
-      Hey, {user.email}!
-      <form action={signOutAction}>
-        <Button type='submit' variant={'outline'}>
-          Sign out
-        </Button>
-      </form>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <p className='text-sm'>{user.email}</p>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Your account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href='/reset-password' className='px-4'>
+            Change password
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <form action={signOutAction}>
+            <button type='submit' className='px-4'>
+              Sign out
+            </button>
+          </form>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   ) : (
     <div className='flex gap-2'>
       <Button asChild size='sm' variant='outline'>
